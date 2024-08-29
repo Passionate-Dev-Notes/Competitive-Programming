e163f6944be09bc7200f788ed6383d98db8f6dfc
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-08-29 22:58:53 +0530

    Check all prime number in particular range

--- primeNumber/PrimeNumberInRange.java
+++ primeNumber/PrimeNumberInRange.java
@@ -3,7 +3,11 @@ package primeNumber;
 public class PrimeNumberInRange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PrimeNumberInRangeLogic rPrime = new PrimeNumberInRangeLogic();
+		int result[]=rPrime.CheckAllPrimeRec(5, 20);
+		for(int a: result) {
+			System.out.print(a+" ");
+		}
 
 	}
 
--- primeNumber/PrimeNumberInRangeLogic.java
+++ primeNumber/PrimeNumberInRangeLogic.java
@@ -1,5 +1,35 @@
 package primeNumber;
 
-public class PrimeNumberInRangeLogic {
 
-}
+public class PrimeNumberInRangeLogic {
+	
+
+	public int[] CheckAllPrimeRec(int first, int last) {
+		int result[]= new int[(first+last)/2];
+		int index=0;
+		
+			for(int i = first; i<=last;i++) {
+				int j =2;
+				boolean check=false;
+				//i=(int)Math.sqrt(i);
+				while(j <= (i/2)) {
+					if(i%j == 0) {
+						check = false;
+						break;
+					}
+					else
+						check = true;
+					j++;
+				}
+				if(check) {
+					result[index] = i;
+					index++;
+				}
+			}
+		
+		
+		return result;
+	}
+	}
+
+

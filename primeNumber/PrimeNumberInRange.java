ea0a4f87b0ed7841e4928cbc9b572694de42b240
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-08-30 15:54:14 +0530

    Check all PrimeNumber in user range

--- primeNumber/PrimeNumberInRange.java
+++ primeNumber/PrimeNumberInRange.java
@@ -1,14 +1,18 @@
 package primeNumber;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class PrimeNumberInRange {
 
 	public static void main(String[] args) {
 		PrimeNumberInRangeLogic rPrime = new PrimeNumberInRangeLogic();
-		int result[]=rPrime.CheckAllPrimeRec(5, 20);
-		for(int a: result) {
-			System.out.print(a+" ");
+		List<Integer> ltr =rPrime.CheckAllPrimeRec(1, 20);
+		Iterator it = ltr.iterator();
+		
+		while(it.hasNext()) {
+			System.out.print(it.next()+" ");
 		}
-
 	}
 
 }
--- primeNumber/PrimeNumberInRangeLogic.java
+++ primeNumber/PrimeNumberInRangeLogic.java
@@ -1,29 +1,37 @@
 package primeNumber;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeNumberInRangeLogic {
 	
 
-	public int[] CheckAllPrimeRec(int first, int last) {
-		int result[]= new int[(first+last)/2];
+	public List<Integer> CheckAllPrimeRec(int first, int last) {
+		List<Integer> result= new ArrayList<Integer>();
 		int index=0;
 		
 			for(int i = first; i<=last;i++) {
 				int j =2;
 				boolean check=false;
-				//i=(int)Math.sqrt(i);
-				while(j <= (i/2)) {
-					if(i%j == 0) {
-						check = false;
-						break;
+				if(i== 1) 
+					check = false;
+				else if(i==2)
+					check = true;
+				else {
+					while(j <= (i/2)) {
+						if(i%j == 0) {
+							check = false;
+							break;
+						}
+						else
+							check = true;
+						j++;
 					}
-					else
-						check = true;
-					j++;
 				}
+				
 				if(check) {
-					result[index] = i;
-					index++;
+					result.add(i);
+					
 				}
 			}
 		

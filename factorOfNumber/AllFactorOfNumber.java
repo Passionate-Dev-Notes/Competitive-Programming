2051060f9cac7286bce9cf920c843a1fc21cb09c
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-09-02 20:19:02 +0530

    factorial concept

--- factorOfNumber/AllFactorOfNumber.java
+++ factorOfNumber/AllFactorOfNumber.java
@@ -4,7 +4,7 @@ public class AllFactorOfNumber {
 
 	public static void main(String[] args) {
 		FindAllFactorOfNumber fact = new FindAllFactorOfNumber();
-		fact.findFactor(40);
+		fact.findFactor(34);
 
 	}
 

c3ca4a602ab5656ab00155c87f989d8ae2dfa1b5
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-09-04 22:57:42 +0530

    Find out lcm of any two number

---
+++ lcmOrLeastCommonMultiple/LCMLogic.java
@@ -0,0 +1,5 @@
+package lcmOrLeastCommonMultiple;
+
+public class LCMLogic {
+
+}
---
+++ lcmOrLeastCommonMultiple/LCMOrLeastCommonMultiple.java
@@ -0,0 +1,10 @@
+package lcmOrLeastCommonMultiple;
+
+public class LCMOrLeastCommonMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

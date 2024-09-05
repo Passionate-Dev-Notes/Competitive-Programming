95efdcc4e24c612e1efe2627a11e1c6a89871261
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-09-05 22:45:12 +0530

    Calculating LCM of Any Two number

--- lcmOrLeastCommonMultiple/LCMLogic.java
+++ lcmOrLeastCommonMultiple/LCMLogic.java
@@ -1,5 +1,21 @@
 package lcmOrLeastCommonMultiple;
 
 public class LCMLogic {
+	public int lcmLogic(int n1, int n2) {
+		int multipleStor =1;
+		for(int i =2; i<n1/2;i++) {
+			while(n1%i==0 && n2%i==0) {
+					multipleStor = multipleStor*i;
+					n1 = n1/i;
+					n2 = n2/i;
+			}
+			
+		}
+		if(n1!=0 || n2 !=0) {
+			multipleStor=multipleStor*n1*n2;
+		}
+		return multipleStor;
+
+	}
 
 }
--- lcmOrLeastCommonMultiple/LCMOrLeastCommonMultiple.java
+++ lcmOrLeastCommonMultiple/LCMOrLeastCommonMultiple.java
@@ -1,9 +1,19 @@
 package lcmOrLeastCommonMultiple;
 
+import java.util.Scanner;
+
 public class LCMOrLeastCommonMultiple {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LCMLogic lcm = new LCMLogic();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int n1 = sc.nextInt();
+		System.out.println("Enter second number");
+		int n2 = sc.nextInt();
+		
+		int result = lcm.lcmLogic(n1, n2);
+		System.out.println("Lcm of two Number is: "+result);
 
 	}
 

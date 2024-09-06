fb276a27abad1d9c7643244e3460de994a9aef8f
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-09-06 22:43:01 +0530

    Ascii value of Character

---
+++ assciiValue/ASCIIValueOfCharacter.java
@@ -0,0 +1,12 @@
+package assciiValue;
+
+public class ASCIIValueOfCharacter {
+
+	public static void main(String[] args) {
+		LogicOFASCIIValue ascii = new LogicOFASCIIValue();
+		int result = ascii.checkAsciiValue("A");
+		System.out.println(result);
+
+	}
+
+}
---
+++ assciiValue/LogicOFASCIIValue.java
@@ -0,0 +1,11 @@
+package assciiValue;
+
+public class LogicOFASCIIValue {
+	public int checkAsciiValue(char str) {
+		//int n = str.charAt(0); // if string tp character 
+		int n = str;  // if we store character variable into int then it will 
+		             //              give ASCII value
+		return n;
+	}
+
+}

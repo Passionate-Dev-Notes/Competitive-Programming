73d7d42a3f0845a7d07712d956c9ac3b7de97d0d
Passionate-Dev-Notes <kr.riturajsingh@gmail.com>
2024-09-30 23:07:29 +0530

    Fraction Sum

---
+++ fractionSum/SumOfTwoFraction.java
@@ -0,0 +1,9 @@
+package fractionSum;
+
+public class SumOfTwoFraction {
+
+	public static void main(String[] args) {
+		SumOfTwoFractionLogic obj = new 
+	}
+
+}
---
+++ fractionSum/SumOfTwoFractionLogic.java
@@ -0,0 +1,9 @@
+package fractionSum;
+
+public class SumOfTwoFractionLogic {
+	public int fractionSum(int n1, int n2) {
+		
+		return 0;
+	}
+
+}
